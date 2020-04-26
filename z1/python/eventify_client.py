7e19402bc641567a8f5b5a3bfc1e8349a12439af
Piotr Matląg <piotr.matlag@protonmail.com>
2020-04-26 20:34:19 +0200

    Handle network failures

--- z1/python/eventify_client.py
+++ z1/python/eventify_client.py
@@ -1,6 +1,7 @@
 import sys
 import asyncio
 import grpc
+import time
 import eventify_pb2 as eventify
 import eventify_pb2_grpc as eventify_grpc
 
@@ -9,12 +10,20 @@ async def subscribe_to_type(stub, type_name):
     print(subscription_string)
     print("-" * len(subscription_string))
 
+    reconnect_interval = 1
     request = eventify.SubRequest(type=string_to_event_type(type_name))
 
-    events = stub.Subscribe(request)
+    while True:
+        try:
+            events = stub.Subscribe(request)
 
-    for event in events:
-        print("New %s event: %s" % (type_name, event))
+            for event in events:
+                print("New %s event: %s" % (type_name, event))
+                reconnect_interval = 1  # Got connection, reset timer
+        except grpc.RpcError as e:
+            print("Connectivity issue: %s" % e.details())
+            time.sleep(reconnect_interval)
+            reconnect_interval = increase_interval(reconnect_interval)
 
 def string_to_event_type(type_name):
     event_types = {
@@ -25,10 +34,16 @@ def string_to_event_type(type_name):
 
     return event_types[type_name.upper()]
 
+def increase_interval(interval):
+    new_interval = interval * 2
+    if new_interval > 30:
+        new_interval = 30
+
+    return new_interval
+
 async def main():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = eventify_grpc.EventifyStub(channel)
-        print(sys.argv[1:])
         coroutines = []
         for event_type in sys.argv[1:]:
             coroutines.append(subscribe_to_type(stub, event_type))
